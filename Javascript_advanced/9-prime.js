6a91f2ef9aa902e0bd52b125c0edc64b042132c4
EliBrank <eliasbrinkman@gmail.com>
2024-11-19 22:08:05 -0600

    create function to count number of prime numbers, use performance.now() to measure execution time

---
+++ Javascript_advanced/9-prime.js
@@ -0,0 +1,35 @@
+// function to check if input is prime
+function isPrime(n) {
+  if (n < 2) {
+    return false;
+  }
+
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function countPrimeNumbers(upperLimit = 100) {
+  let numPrimeNumbers = 0;
+
+  for (let i = 2; i < upperLimit; i++) {
+    if (isPrime(i)) {
+      numPrimeNumbers++
+    }
+  }
+
+  return numPrimeNumbers;
+}
+
+const startTime = performance.now();
+
+countPrimeNumbers();
+
+const endTime = performance.now();
+const executionTime = endTime - startTime;
+
+console.log(`Execution time of printing countPrimeNumbers was ${executionTime} milliseconds.`);
